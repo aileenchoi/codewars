a1e401cf5125876fa9e0a697fe06526ed75fccde
Aileen Choi <ac.aileenchoi@gmail.com>
2022-07-29 15:18:10 -0400

    change folders layout

--- 8kyu/5-25-2022_reverse-str.js
+++ 8kyu/5-25-2022_reverse-str.js
@@ -1,14 +1,3 @@
-function removeChar(str){
-    let newStr = str.slice(1,str.length-1)
-    return newStr
-   };
-   
-   const removeChar = str => str.slice(1,-1)
-
-console.log(removeChar('eloquent'))//loquen
-console.log(removeChar('tired'))//ire
-console.log(removeChar('sushi'))//ush
-
 // Reversed Strings
 // Complete the solution so that it reverses the string passed into it.
 
